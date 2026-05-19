f658a9b8b513f4d364818b52eaaec86ccf546ff1
Mona <monaa.alaa.1414@gmail.com>
2026-05-18 21:05:37 -0500

    fix s3 directory reference

--- src/ingestion/load_tlc.py
+++ src/ingestion/load_tlc.py
@@ -6,7 +6,7 @@ spark = SparkSession.builder \
     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4") \
     .getOrCreate()
 
-S3_BUCKET = "s3://de300-project7/raw/tlc/"
+S3_BUCKET = "s3a://de300-project7/raw/tlc/"
 
 df = spark.read.parquet(S3_BUCKET)
 
