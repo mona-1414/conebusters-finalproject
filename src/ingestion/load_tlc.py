eb9ed6a17f01f5603f3be163d1f5875dbb6957bf
Mona <monaa.alaa.1414@gmail.com>
2026-05-18 21:07:08 -0500

    small fix for pyspark job config

--- src/ingestion/load_tlc.py
+++ src/ingestion/load_tlc.py
@@ -1,9 +1,12 @@
 from pyspark.sql import SparkSession
-import os
 
 spark = SparkSession.builder \
     .appName("TLC Data Ingestion") \
     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .config("spark.hadoop.fs.s3a.connection.timeout", "60000") \
+    .config("spark.hadoop.fs.s3a.connection.establish.timeout", "60000") \
     .getOrCreate()
 
 S3_BUCKET = "s3a://de300-project7/raw/tlc/"
